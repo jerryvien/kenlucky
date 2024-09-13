402f5448f529480dd5792e4e6239fed6b6ee2ab2
jerryvien <jerryyee0902@gmail.com>
2024-09-13 16:12:11 +0800

    Deleted for new project

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-    <footer class="bg-primary text-white text-center py-3 mt-auto">
-        <p>All rights reserved © 2024 by Ken Group</p>
-    </footer>
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Location: /pages/login.php");
-#Testing Comment session#
-#testing after putting in password#
-exit;
-?>
